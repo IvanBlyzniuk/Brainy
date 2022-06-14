03b5277bef504be2dd4c8f13c27629e197b603f1
Ivan Blyzniuk <ivan.blyzniuk@ukma.edu.ua>
2022-06-14 21:14:16 +0300

    Added pause menu

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    public GameObject pauseButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Pause()
+    {
+        pauseButton.SetActive(false);
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseButton.SetActive(true);
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
