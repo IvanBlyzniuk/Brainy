bf7fb4ce12f35524e24b8ed5caf234cbd58ffd1d
Ivan Blyzniuk <ivan.blyzniuk@ukma.edu.ua>
2022-06-25 17:58:17 +0300

    Added stop playing screen

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -16,13 +16,12 @@ public class MainMenuController : MonoBehaviour
     {
         volume = PlayerPrefs.GetFloat("Volume");
         isSoundInitialized = PlayerPrefs.GetInt("IsSoundInitialized");
-        //Debug.Log(isSoundInitialized);
-        if(isSoundInitialized == 0)
+        if (isSoundInitialized == 0)
         {
             volume = 0.5f;
             PlayerPrefs.SetInt("IsSoundInitialized", 1);
         }
-        volumeText.text = ((int)(volume*10)).ToString();
+        volumeText.text = ((int)(volume * 10)).ToString();
         Debug.Log(volume);
         if (Time.realtimeSinceStartup > 10)
         {
@@ -31,78 +30,59 @@ public class MainMenuController : MonoBehaviour
     }
     public void PlayBridgeGame()
     {
-        if (!stopPlayingScreen.activeSelf)
-        {
-
-        
-        PlayerPrefs.SetFloat("Volume",volume);
+        PlayerPrefs.SetFloat("Volume", volume);
         SceneManager.LoadScene("BridgeGame");
-        }
     }
     public void PlayBubbleGame()
     {
-        if (!stopPlayingScreen.activeSelf)
-        {
-            PlayerPrefs.SetFloat("Volume", volume);
-            SceneManager.LoadScene("Bubble game");
-        }
+        PlayerPrefs.SetFloat("Volume", volume);
+        SceneManager.LoadScene("Bubble game");
     }
     public void PlayWindowGame()
     {
-        if (!stopPlayingScreen.activeSelf)
-        {
-            PlayerPrefs.SetFloat("Volume", volume);
-            SceneManager.LoadScene("Window Game");
-        }
+        PlayerPrefs.SetFloat("Volume", volume);
     }
     public void PlayOrchestralGame()
     {
         PlayerPrefs.SetFloat("Volume", volume);
         SceneManager.LoadScene("Orchestral game");
-        
+
     }
     public void GoToScoreScreen()
     {
-        if (!stopPlayingScreen.activeSelf)
-        {
-            SceneManager.LoadScene("Score Menu");
-        }
+        SceneManager.LoadScene("Score Menu");
     }
 
     public void ExitGame()
     {
-        if (!stopPlayingScreen.activeSelf)
-        {
-            Debug.Log("Exit completed");
-            Application.Quit();
-        }
+        Debug.Log("Exit completed");
+        Application.Quit();
     }
 
     public void IncreaseVolume()
     {
-        if (!stopPlayingScreen.activeSelf)
+        if (volume < 1)
         {
-            if (volume < 1)
-            {
-                volume += 0.1f;
-                volumeText.text = ((int)(volume * 10)).ToString();
-            }
+            volume += 0.1f;
+            volumeText.text = ((int)(volume * 10)).ToString();
         }
     }
     public void DecreaseVolume()
     {
-        if (!stopPlayingScreen.activeSelf)
+        if (volume > 0)
         {
-            if (volume > 0)
-            {
-                volume -= 0.1f;
-                volumeText.text = ((int)(volume * 10)).ToString();
-            }
-            if (volume < 0.1f)
-            {
-                volume = 0;
-                volumeText.text = ((int)(volume * 10)).ToString();
-            }
+            volume -= 0.1f;
+            volumeText.text = ((int)(volume * 10)).ToString();
         }
+        if (volume < 0.1f)
+        {
+            volume = 0;
+            volumeText.text = ((int)(volume * 10)).ToString();
+        }
+    }
+
+    public void ContinueRegardless()
+    {
+        stopPlayingScreen.SetActive(false);
     }
 }
